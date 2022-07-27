617da53de1768aaf207603c4c83119df2df8303e
Sean Olson <SloughHand@gmail.com>
2022-07-27 10:15:20 -0700

    additional comments to index.js for  direction

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const { discover_the_train_tickets } = require('./helpers')
 
 function sort_tickets(the_evidence) {
-
-    // code your sorting function here and return the sorted tickets
+    /* 
+      Code your sorting function here and return the sorted tickets.
+      By default it returns the unsorted stack.
+    */
     const the_goods = the_evidence
     return the_goods
 }
 
+
+/* 
+  This variable determines the number of hops in the trip, 
+  or tickets in the stack.  It can range from 2 to 50. 
+*/
 const ticket_count = 50
 
-// this is the controling function that calls the 
+/* 
+  This is the controling function that calls the sort function.
+  By default it logs the stack return from sort function to the console.
+  Make any changes you feel necessary.
+*/
 function crack_the_case() {
     const the_evidence = discover_the_train_tickets(ticket_count)
     const the_goods = sort_tickets(the_evidence)
@@ -17,4 +28,3 @@ function crack_the_case() {
 }
 
 crack_the_case()
-
